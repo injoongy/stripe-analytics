42768f2ab35422d4c7209a700aca85a2d36fde69
Injoong Yoon <27440940+injoongy@users.noreply.github.com>
2025-11-25 11:05:23 -0800

    feat: dockerize the whole app

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   images: {
     domains: ["dwdwn8b5ye.ufs.sh"]
   }
